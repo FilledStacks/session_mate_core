e78166c69960cb6c143e7a2f83b3683c41ae75f9
Fernando Ferrara <ferrarafer@gmail.com>
2023-08-15 22:46:12 -0300

    fix: export session on package

--- lib/session_mate_core.dart
+++ lib/session_mate_core.dart
@@ -6,4 +6,5 @@ export 'src/interfaces/session_event.dart';
 export 'src/models/event_position.dart';
 export 'src/models/request_event.dart';
 export 'src/models/response_event.dart';
+export 'src/models/session.dart';
 export 'src/models/ui_event.dart';
